3e86ca6358baaec9cc859b91fadf3205e4a71db7
manuel <manuel@darth-vader.(none)>
2011-07-03 15:56:26 -0600

    Agregado Blab! IM al proyecto // Modificado el Chat controller para conectarse al chat de Blab

--- amy_editor/trunk/client/controller.chat.js
+++ amy_editor/trunk/client/controller.chat.js
@@ -22,12 +22,17 @@ client.controller.renderChat = function(force)
 {
 	var node = client.nodes.chatSection;
 	node.rc();
-	var width = node.w()-20;
+	var width = node.w()-2000;
+	var height = node.h()-25;
 	var node_coll = node.a($$()).s('padding:8px;margin:4px;').w(width);
 	var pane_index = -1;
 	var label = '...';
-	var ht = '<div style="margin-top:6px;padding:3px;background:#f0f0f0;-moz-border-radius:3px;-webkit-border-radius:3px;border:1px solid #ddd"><table cellspacing="0" width="95%"><tbody>';
-	ht += '</tbody></table>THIS IS A SAMPLE CHAT SECTION</div>';
+	var ht = '<div style="background:#f0f0f0;-moz-border-radius:3px;-webkit-border-radius:3px;border:1px solid #ddd">';
+	ht += '<iframe height=';
+	ht += height;
+	ht +=' width=100%';
+	//ht += width;
+	ht += ' src="/blab_im/chat.php?u=1"> </div>';
 	node_coll.a($$()).t(ht);
 	client.nodes.chatSection.v(true)
 }
